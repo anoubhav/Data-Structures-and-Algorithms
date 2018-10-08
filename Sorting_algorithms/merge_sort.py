3527aa18b38bbd99e6cf7a08c77ed11a5dc80d76
Anoubhav Agarwaal <anoubhav.agarwaal@gmail.com>
2018-10-08 22:14:38 +0530

    added comments to merge_sort.py

--- Sorting_algorithms/merge_sort.py
+++ Sorting_algorithms/merge_sort.py
@@ -1,8 +1,11 @@
 def create_array(size=10, max=50):
+    """ Returns random array of given size and elements upto max
+    (int, int) -> (list) """
     from random import randint
     return [randint(0, max) for _ in range(size)]
 
 def merge(a, b):
+    """ Conquer routine(does all the work)"""
     c = [] # final output array
     a_idx, b_idx = 0, 0
     while a_idx<len(a) and b_idx<len(b):
@@ -12,11 +15,14 @@ def merge(a, b):
         else:
             c.append(b[b_idx])
             b_idx += 1
-    if a_idx==len(a): c.extend(b[b_idx:])
-    else: c.extend(a[a_idx:])
+    # if a_idx==len(a): c.extend(b[b_idx:])
+    # else: c.extend(a[a_idx:])
+    c += a[a_idx:]
+    c += b[b_idx:]
     return c
 
 def merge_sort(a):
+    """ Divide routine """
     # a list of 0 or 1 element is sorted by definition
     if len(a)<=1: return a
 
